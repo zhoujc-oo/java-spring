57d2ee69eaf88b6987e4b29f68dec9de3ff24e44
zhoujc <84381853+zhoujc-oo@users.noreply.github.com>
2021-06-10 23:37:13 +0800

    Add files via upload
    jdbc的多元数据

---
+++ spring-boot-jdbctemplate-MultiDataSource/src/main/java/springboot/dao/MysqlStudentDao.java
@@ -0,0 +1,9 @@
+package springboot.dao;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface MysqlStudentDao {
+    List<Map<String, Object>> getAllStudents();
+}
---
+++ spring-boot-jdbctemplate-MultiDataSource/src/main/java/springboot/dao/OracleStudentDao.java
@@ -0,0 +1,8 @@
+package springboot.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OracleStudentDao {
+    List<Map<String, Object>> getAllStudents();
+}
